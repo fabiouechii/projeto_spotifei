11af7fe09d81d9e62af60acc29ea2001de3f5efd
fabiouechii <fabiouechi19@gmail.com>
2025-05-15 22:05:19 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/projeto_spotifei/Projeto_spotifei.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_spotifei;
+
+import view.CadastroFrame;
+import view.LoginFrame;
+import view.MenuFrame;
+import view.PesquisarFrame;
+
+/**
+ *
+ * @author Fabio
+ */
+public class Projeto_spotifei {
+
+    public static void main(String[] args) {
+        PesquisarFrame janela = new PesquisarFrame();
+        janela.setVisible(true);
+    }
+}
